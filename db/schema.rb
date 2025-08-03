0799ec1fe7a9a26d1008524cb0160feed1a19478
Junior <eecfredes@gmail.com>
2025-08-02 21:48:30 -0300

    :rocket: schemas update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_02_020259) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_03_002728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_02_020259) do
     t.integer "limit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "books", force: :cascade do |t|
